ee685b5633f76992f436858c5fd207787c02519f
maXss-tech <denzelcordovez9@gmail.com>
2026-09-04 19:19:03 +0800

    Create config.example.php

---
+++ config.example.php
@@ -0,0 +1,8 @@
+<?php
+// Copy this to config.php and add your real credentials
+$host = "localhost";
+$user = "your_username";
+$pass = "your_password";
+$db = "your_database";
+$conn = new mysqli($host, $user, $pass, $db);
+?>
